0a2280954ecbfaebebf50cd04ca22bd9170a8a7a
Dylan Dove <dove@berkeley.edu>
2025-04-02 20:09:42 -0700

    New stuff

--- main.py
+++ main.py
@@ -551,7 +551,7 @@ async def handle_media_stream(websocket: WebSocket):
                 session_update = {
                     "type": "session.update",
                     "session": {
-                        "turn_detection": {"type": "server_vad"},
+                        "turn_detection": {"type": "semantic_vad", },
                         "input_audio_format": "g711_ulaw",
                         "output_audio_format": "g711_ulaw",
                         "voice": VOICE,
